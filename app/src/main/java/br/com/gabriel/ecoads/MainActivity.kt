758451292432da007cec3e287e7dfd7646604cd7
Gabriel Reis <gabrielgomesreis23@gmail.com>
2023-03-25 13:55:23 -0300

    Modificando o FindById para viewBinding

--- app/src/main/java/br/com/gabriel/ecoads/MainActivity.kt
+++ app/src/main/java/br/com/gabriel/ecoads/MainActivity.kt
@@ -1,21 +1,24 @@
 package br.com.gabriel.ecoads
 
 import android.content.Intent
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.util.Log
 import android.widget.Button
 import android.widget.EditText
+import androidx.appcompat.app.AppCompatActivity
+import br.com.gabriel.ecoads.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
+    private lateinit var binding: ActivityMainBinding
     private lateinit var botao: Button
     private lateinit var campo: EditText
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
-        botao = findViewById(R.id.main_button_enviar)
-        campo = findViewById(R.id.main_input_texto)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+        botao = binding.mainButtonEnviar
+        campo = binding.mainInputTexto
 
         botao.setOnClickListener {
             val texto = campo.text.toString()
--- app/src/main/java/br/com/gabriel/ecoads/ResultActivity.kt
+++ app/src/main/java/br/com/gabriel/ecoads/ResultActivity.kt
@@ -1,22 +1,27 @@
 package br.com.gabriel.ecoads
 
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.util.Log
 import android.widget.TextView
+import androidx.appcompat.app.AppCompatActivity
+import br.com.gabriel.ecoads.databinding.ActivityResultBinding
 import com.google.android.material.floatingactionbutton.FloatingActionButton
 
 class ResultActivity : AppCompatActivity() {
+    private lateinit var binding: ActivityResultBinding
     private lateinit var rotulo: TextView
     private lateinit var botaoResult: FloatingActionButton
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_result)
         val valor = intent.getStringExtra("MAIN_KEY_TEXTO")
         Log.i("ResultActivity","Chegou texto:" + valor)
-        rotulo = findViewById(R.id.activity_result_texto)
-        botaoResult = findViewById(R.id.activity_result_fab)
+
+        binding = ActivityResultBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        rotulo = binding.activityResultTexto
+        botaoResult = binding.activityResultFab
 
         rotulo.text = valor
         botaoResult.setOnClickListener {
